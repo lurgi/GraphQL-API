91e67a2d32336cde4e4ca60260135230e3c6b6bc
lurgi <wahoo9040@naver.com>
2023-08-28 21:16:35 +0900

    0828

--- server.js
+++ server.js
@@ -1,6 +1,104 @@
 import { ApolloServer, gql } from "apollo-server";
 
-const server = new ApolloServer({});
+let tweets = [
+  {
+    id: "1",
+    text: "hello",
+    userId: "1",
+  },
+  {
+    id: "2",
+    text: "second one",
+    userId: "2",
+  },
+  {
+    id: "3",
+    text: "third party",
+    userId: "2",
+  },
+];
+
+let users = [
+  {
+    id: "1",
+    firstName: "jeongWoo",
+    lastName: "Park",
+  },
+  {
+    id: "2",
+    firstName: "Uran",
+    lastName: "Kim",
+  },
+];
+
+const typeDefs = gql`
+  type User {
+    id: ID!
+    firstName: String!
+    lastName: String!
+    fullName: String!
+  }
+  """
+  여기에 Tweet의 설명을 적습니다.
+  """
+  type Tweet {
+    id: ID!
+    text: String!
+    author: User!
+  }
+  type Query {
+    allUsers: [User!]!
+    allTweets: [Tweet!]!
+    tweet(id: ID): Tweet
+  }
+  type Mutation {
+    postTweet(text: String!, userId: ID!): Tweet!
+    deleteTweet(id: ID!): Boolean!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    allTweets() {
+      return tweets;
+    },
+    tweet(root, { id }) {
+      return tweets.find((tweet) => tweet.id == id);
+    },
+    allUsers() {
+      return users;
+    },
+  },
+  Mutation: {
+    postTweet(_, { text, userId }) {
+      const newTweet = {
+        id: tweets[tweets.length - 1].id + 1,
+        text,
+        author: userId,
+      };
+      tweets.push(newTweet);
+      return newTweet;
+    },
+    deleteTweet(_, { id }) {
+      const tweet = tweets.find((tweet) => tweet.id === id);
+      if (!tweet) return false;
+      tweets = tweets.filter((tweet) => tweet.id !== id);
+      return true;
+    },
+  },
+  User: {
+    fullName({ firstName, lastName }) {
+      return firstName + lastName;
+    },
+  },
+  Tweet: {
+    author({ userId }) {
+      return users.find((user) => user.id === userId);
+    },
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
 
 server.listen().then(({ url }) => {
   console.log(`Running on ${url}`);
